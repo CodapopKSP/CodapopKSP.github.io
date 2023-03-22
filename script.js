99514a21a4a8948ebdf77e0ca2f74a0ad6d011a8
Codapop <george.dikun91@gmail.com>
2023-03-22 23:38:10 +0800

    touch

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ draggables.forEach(draggable => {
     draggable.style.left = (touch.clientX - offsetX) + 'px';
     draggable.style.top = (touch.clientY - offsetY) + 'px';
     alert(`touchstart: ${touch.clientX}, ${touch.clientY}, ${rect.left}, ${rect.top}`);
-    alert(`touchmove: ${touch.clientX}, ${touch.clientY}, ${draggable.style.left}, ${draggable.style.top}`);*/
+    alert(`touchmove: ${touch.clientX}, ${touch.clientY}, ${draggable.style.left}, ${draggable.style.top}`);
   });
   /*
   draggable.addEventListener('touchmove', (event) => {
