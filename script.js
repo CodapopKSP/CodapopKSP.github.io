faefca59306ed5321623646a8c75126a59c2a35f
Codapop <george.dikun91@gmail.com>
2023-03-22 13:00:27 +0800

    Change touch.

--- script.js
+++ script.js
@@ -93,25 +93,22 @@ draggables.forEach(draggable => {
     draggable.classList.add('dragging');
     const touch = event.touches[0];
     event.dataTransfer.setData('text/plain', this.id);
-    // Save the initial touch position for later use
-    draggable.dataset.touchStartX = touch.clientX
-    draggable.dataset.touchStartY = touch.clientY
+    // Set the position of the draggable element to the position of the touch
+    draggable.style.left = touch.clientX + 'vh';
+    draggable.style.top = touch.clientY + 'vh';
   });
-  
+
   draggable.addEventListener('touchmove', (event) => {
     // Prevent the default behavior to avoid scrolling while dragging
     event.preventDefault();
     const touch = event.touches[0];
-    // Calculate the distance traveled since the touch started
-    const deltaX = touch.clientX - draggable.dataset.touchStartX;
-    const deltaY = touch.clientY - draggable.dataset.touchStartY;
-    // Move the draggable element using CSS transforms
-    draggable.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    // Move the draggable element to the position of the touch
+    draggable.style.left = touch.clientX + 'px';
+    draggable.style.top = touch.clientY + 'px';
   });
-  
+
   draggable.addEventListener('touchend', () => {
     draggable.classList.remove('dragging')
-    draggable.style.transform = ''; // Clear the CSS transform
     updateTotalPrice()
   })
 })
