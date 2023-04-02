9955f8c27e5f3d7fe1b1cca04e3945b1b9d2ade3
Codapop <george.dikun91@gmail.com>
2023-04-02 15:30:05 +0800

    Add robust engineer's report.

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ recommendedConfigsBtn.addEventListener('click', function() {
 const engineersReport = document.getElementById('report');
 engineersReport.addEventListener('click', function() {
   let reportContent = '';
-
   const boxes = document.querySelectorAll('.box');
   let totalPrice = 0;
   let isSizeMismatch = false;
@@ -47,16 +46,16 @@ engineersReport.addEventListener('click', function() {
     const draggables = box.querySelectorAll('.draggable');
     let boxPrice = parseFloat(box.getAttribute('data-price'));
     totalPrice += boxPrice;
+    // Make sure all module docks are filled.
     if (draggables.length !== parseInt(box.getAttribute('data-size'))) {
       isSizeMismatch = true;
-    } else {
-
-      draggables.forEach(draggable => {
-        const price = parseFloat(draggable.getAttribute('data-price'));
-        totalPrice += price;
-      });
     }
-
+    // Add the price of each draggable
+    draggables.forEach(draggable => {
+      const price = parseFloat(draggable.getAttribute('data-price'));
+      totalPrice += price;
+    });
+    // Show price.
     if (boxPrice > 0) {
       reportContent += `<p style="color: #4d9de3;"><strong>${box.getAttribute('data-name')}</strong> - $${boxPrice.toFixed(2)}</p>`;
       draggables.forEach(draggable => {
@@ -68,123 +67,169 @@ engineersReport.addEventListener('click', function() {
       reportContent += `<p style="color: #df7070;">${box.getAttribute('data-name')} - <em>No items selected</em></p>`;
       reportContent += `<hr>`;
     }
-
-    
   });
 
-  // Add recommendation section
-  let recommendation = '';
-  let hasThrottle = false;
-  let controlsys_duplicate = false;
-  let executive_duplicate = false;
-  let hasRotation = false;
-  let hasTranslation = false;
-  let rotation_duplicate = false;
-  let translation_duplicate = false;
-  let throttle_duplicate = false;
-  let telemetry_overload = false;
-  let nav_time = false;
-  let navTime = false;
+  // Add booleanas for modules and calculate if there are any conflicts
+  var hasThrottle = false;
+  var controlsys_duplicate = false;
+  var executive_duplicate = false;
+  var hasRotation = false;
+  var hasTranslation = false;
+  var rotation_duplicate = false;
+  var translation_duplicate = false;
+  var throttle_duplicate = false;
+  var telemetry_overload = false;
+  var nav_time = false;
+  var navTime = false;
+
+  var has_analog_throttle = false;
+  var has_rotation_throttle = false;
+  var has_throttle = false;
+  var has_translation = false;
+  var has_analog = false;
+  var has_rotation = false;
+  var has_control_sys = false;
+  var has_executive_control = false;
+  var has_stage = false;
+  var has_executive_groups = false;
+  var has_executive = false;
+  var has_nav = false;
+  var has_time = false;
+  var has_navTime = false;
+
+  // Check all modules
   boxes.forEach(box => {
-    const has_analog_throttle = box.querySelector('#f2');
-    const has_rotation_throttle = box.querySelector('#f4');
-    const has_throttle = box.querySelector('#f6');
-    const has_translation = box.querySelector('#f5');
-    const has_analog = box.querySelector('#f1');
-    const has_rotation = box.querySelector('#f3');
-
-    if (has_rotation_throttle || has_rotation || has_analog || has_analog_throttle) {
-      hasRotation = true;
+    if (!has_analog_throttle) {
+      has_analog_throttle = box.querySelector('#f2');
+    }
+    if (!has_rotation_throttle) {
+      has_rotation_throttle = box.querySelector('#f4');
+    }
+    if (!has_throttle) {
+      has_throttle = box.querySelector('#f6');
+    }
+    if (!has_translation) {
+      has_translation = box.querySelector('#f5');
+    }
+    if (!has_analog) {
+      has_analog = box.querySelector('#f1');
+    }
+    if (!has_rotation) {
+      has_rotation = box.querySelector('#f3');
+    }
+    if (!has_control_sys) {
+      has_control_sys = box.querySelector('#g1');
+    }
+    if (!has_executive_control) {
+      has_executive_control = box.querySelector('#a5');
+    }
+    if (!has_stage) {
+      has_stage = box.querySelector('#a2');
+    }
+    if (!has_executive_groups) {
+      has_executive_groups = box.querySelector('#a4');
+    }
+    if (!has_executive) {
+      has_executive = box.querySelector('#a3');
+    }
+    if (!has_nav) {
+      has_nav = box.querySelector('#b2');
+    }
+    if (!has_time) {
+      has_time = box.querySelector('#b1');
+    }
+    if (!has_navTime) {
+      has_navTime = box.querySelector('#b3');
     }
 
-    if (has_translation || has_analog || has_analog_throttle) {
-      hasTranslation = true;
-    }
-
-    if (has_analog_throttle || has_rotation_throttle || has_throttle) {
-      hasThrottle = true;
-    }
-    
-    const has_control_sys = box.querySelector('#g1');
-    const has_executive_control = box.querySelector('#a5');
-    const has_stage = box.querySelector('#a2');
-    const has_executive_groups = box.querySelector('#a4');
-    const has_executive = box.querySelector('#a3');
-    if (has_control_sys && has_executive_control) {
-      controlsys_duplicate = true;
-    }
-    const exec_elements = [has_executive_control, has_stage, has_executive_groups, has_executive];
-    let numTrue = 0;
-    for (let i = 0; i < exec_elements.length; i++) {
-      if (exec_elements[i]) {
-        numTrue++;
-      }
-      if (numTrue === 2) {
-        executive_duplicate = true;
-        break;
-      }
-    }
-
-    const rot_elements = [has_rotation_throttle, has_rotation, has_analog, has_analog_throttle];
-    numTrue = 0;
-    for (let i = 0; i < rot_elements.length; i++) {
-      if (rot_elements[i]) {
-        numTrue++;
-      }
-      if (numTrue === 2) {
-        rotation_duplicate = true;
-        break;
-      }
-    }
-
-    const trans_elements = [has_translation, has_analog, has_analog_throttle];
-    numTrue = 0;
-    for (let i = 0; i < trans_elements.length; i++) {
-      if (trans_elements[i]) {
-        numTrue++;
-      }
-      if (numTrue === 2) {
-        translation_duplicate = true;
-        break;
-      }
-    }
-
-    const throt_elements = [has_rotation_throttle, has_throttle, has_analog_throttle];
-    numTrue = 0;
-    for (let i = 0; i < throt_elements.length; i++) {
-      if (throt_elements[i]) {
-        numTrue++;
-      }
-      if (numTrue === 2) {
-        throttle_duplicate = true;
-        break;
-      }
-    }
-
-    const has_nav = box.querySelector('#b2');
-    const has_time = box.querySelector('#b1');
-    const has_navTime = box.querySelector('#b3');
-    if ((has_nav || has_time)) {
-      nav_time = true;
-    }
-    if (has_navTime) {
-      navTime = true;
-    }
-
-    const has_ag = box.querySelector('#h1');
-    const has_ag2 = box.querySelector('#h2');
-    const has_ag3 = box.querySelector('#h3');
-    const has_eva = box.querySelector('#d1');
-    const has_telem = box.querySelector('#c1');
-    if (has_telem && (has_ag || has_ag2 || has_ag3 || has_eva || has_control_sys || has_executive_control || has_executive_groups || has_time || has_navTime)) {
+    // Check for modules that conflict with the Telemetry module
+    const has_time_thisbox = box.querySelector('#b1');
+    const has_navTime_thisbox = box.querySelector('#b3');
+    const has_executive_groups_thisbox = box.querySelector('#a4');
+    const has_executive_control_thisbox = box.querySelector('#a5');
+    const has_control_sys_thisbox = box.querySelector('#g1');
+    const has_ag_thisbox = box.querySelector('#h1');
+    const has_ag2_thisbox = box.querySelector('#h2');
+    const has_ag3_thisbox = box.querySelector('#h3');
+    const has_eva_thisbox = box.querySelector('#d1');
+    const has_telem_thisbox = box.querySelector('#c1');
+    if (has_telem_thisbox && (has_ag_thisbox || has_ag2_thisbox || has_ag3_thisbox || has_eva_thisbox || has_control_sys_thisbox || has_executive_control_thisbox || has_executive_groups_thisbox || has_time_thisbox || has_navTime_thisbox)) {
       telemetry_overload = true;
     }
-
   });
-  recommendation = ``;
+  // Check if there are any rotation/translation/throttle controls
+  if (has_rotation_throttle || has_rotation || has_analog || has_analog_throttle) {
+    hasRotation = true;
+  }
+  if (has_translation || has_analog || has_analog_throttle) {
+    hasTranslation = true;
+  }
+  if (has_analog_throttle || has_rotation_throttle || has_throttle) {
+    hasThrottle = true;
+  }
+  
+  // Check if there are any SAS/Executive Actions/NavTime conflicts
+  if (has_control_sys && has_executive_control) {
+    controlsys_duplicate = true;
+  }
+  const exec_elements = [has_executive_control, has_stage, has_executive_groups, has_executive];
+  let numTrue = 0;
+  for (let i = 0; i < exec_elements.length; i++) {
+    if (exec_elements[i]) {
+      numTrue++;
+    }
+    if (numTrue === 2) {
+      executive_duplicate = true;
+      break;
+    }
+  }
+  if ((has_nav || has_time)) {
+    nav_time = true;
+  }
+  if (has_navTime) {
+    navTime = true;
+  }
+
+  // Check if there are any analog conflicts
+  const rot_elements = [has_rotation_throttle, has_rotation, has_analog, has_analog_throttle];
+  numTrue = 0;
+  for (let i = 0; i < rot_elements.length; i++) {
+    if (rot_elements[i]) {
+      numTrue++;
+    }
+    if (numTrue === 2) {
+      rotation_duplicate = true;
+      break;
+    }
+  }
+  const trans_elements = [has_translation, has_analog, has_analog_throttle];
+  numTrue = 0;
+  for (let i = 0; i < trans_elements.length; i++) {
+    if (trans_elements[i]) {
+      numTrue++;
+    }
+    if (numTrue === 2) {
+      translation_duplicate = true;
+      break;
+    }
+  }
+  const throt_elements = [has_rotation_throttle, has_throttle, has_analog_throttle];
+  numTrue = 0;
+  for (let i = 0; i < throt_elements.length; i++) {
+    if (throt_elements[i]) {
+      numTrue++;
+    }
+    if (numTrue === 2) {
+      throttle_duplicate = true;
+      break;
+    }
+  }
+
+  // Construct the warning message based on conflicting modules
+  recommendation = '';
   if (isSizeMismatch || (hasRotation && !hasThrottle) || (hasRotation && !hasTranslation) || (!hasRotation && hasTranslation) || translation_duplicate || rotation_duplicate || throttle_duplicate || executive_duplicate || (nav_time && navTime) || controlsys_duplicate || telemetry_overload) {
     recommendation += `<hr><p style="color: #afe06b;"><strong>Warning:</strong></p>`;
-  } else {
+  } else if (totalPrice > 0){
     recommendation += `<p>This controller has passed all checks.</p>`;
   }
   if (isSizeMismatch) {
@@ -220,12 +265,9 @@ engineersReport.addEventListener('click', function() {
   if (telemetry_overload) {
     recommendation += `<p>You are using the Telemetry module with another module that needs data from Simpit. This will result in the second module's displays not updating properly.</p>`;
   }
-
-
-
-
   recommendation += `<hr>`;
 
+  // Display the final price
   const report = `
     <div>
       ${reportContent}
